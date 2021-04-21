1c97a3cdc41bf5c9986c271bbf605013d53b1805
Junior Martinez <jlmjunior@live.com>
2021-04-21 01:25:26 -0300

    Add project files.

---
+++ DBServer.Project/Data/RestaurantData.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DBServer.Project.Data
+{
+    public class RestaurantData
+    {
+        public IEnumerable<RestaurantModel> GetRestaurants()
+        {
+            return new[]
+            {
+                new RestaurantModel()
+                {
+                    Id = 1,
+                    Name = "Chefe do bairro",
+                    ImageLink = null,
+                    IsAvailable = false
+                },
+                new RestaurantModel()
+                {
+                    Id = 2,
+                    Name = "Teste 2",
+                    ImageLink = null,
+                    IsAvailable = true
+                },
+                new RestaurantModel()
+                {
+                    Id = 3,
+                    Name = "Teste 3",
+                    ImageLink = null,
+                    IsAvailable = true
+                },
+                new RestaurantModel()
+                {
+                    Id = 4,
+                    Name = "Teste 4",
+                    ImageLink = null,
+                    IsAvailable = true
+                }
+            };
+        }
+    }
+}
---
+++ DBServer.Project/Models/RestaurantModel.cs
@@ -0,0 +1,10 @@
+﻿namespace DBServer.Project.Data
+{
+    public class RestaurantModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string ImageLink { get; set; }
+        public bool IsAvailable { get; set; }
+    }
+}
